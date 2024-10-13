6581c83dbf98b22ce008f0acc1a3ac0dfcbaa9cb
Bala Seward <bseward3@gmail.com>
2024-10-13 11:48:49 -0700

    removing wecome message

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import PoseDetectionPage from "./_components/OpenPoseStreamer";
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Pose Detection App</h1>
       {/* Render the Pose Detection component */}
       <PoseDetectionPage />
     </div>
