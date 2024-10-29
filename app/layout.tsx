9d468fbdaf11dc44c57ed9d73520ca16704e6c5d
Bala Seward <bseward3@gmail.com>
2024-10-29 15:49:14 -0700

    formatted file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 // /app/layout.tsx
-import './_assets/globals.css';  // Ensure you have your global styles imported
-import Header from './_components/Header';
+import "./_assets/globals.css"; // Ensure you have your global styles imported
+import Header from "./_components/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ display: 'flex', flexDirection: 'column', height: '100vh', backgroundColor: 'black', color: 'white' }}>
+      <body
+        style={{ display: "flex", flexDirection: "column", height: "100vh", backgroundColor: "black", color: "white" }}
+      >
         <Header />
-        <div style={{ display: 'flex', flex: 1 }}>
-          <main style={{ flex: 1, padding: '20px' }}>{children}</main>
+        <div style={{ display: "flex", flex: 1 }}>
+          <main style={{ flex: 1, padding: "20px" }}>{children}</main>
         </div>
       </body>
     </html>
