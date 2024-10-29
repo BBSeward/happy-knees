cd6fb823b21268b04d7723aeb58465d084e239b6
Bala Seward <bseward3@gmail.com>
2024-10-29 14:32:09 -0700

    working table generation using interface, and properly handling undefined from initialization frames

--- app/_components/OpenPoseStreamer.tsx
+++ app/_components/OpenPoseStreamer.tsx
@@ -11,7 +11,19 @@ interface LandmarkElement {
   visibility?: number;
 }
 
-interface LandmarkResults {
+// Here we will store all values related to the current body geometry given the current and historical model outputs
+interface bodyGeometry {
+  landmark_history: UnpackedLandmarks[]; //todo use this for detecting things over time
+  current_landmarks: UnpackedLandmarks;
+  hip_angle: number;
+  knee_angle: number;
+  ankle_angle: number;
+
+  hip_to_crank_distance: number;
+  hip_to_wrist_distance: number;
+}
+
+interface UnpackedLandmarks {
   nose: LandmarkElement;
   left_eye_inner: LandmarkElement;
   left_eye: LandmarkElement;
@@ -46,7 +58,10 @@ interface LandmarkResults {
   left_foot_index: LandmarkElement;
   right_foot_index: LandmarkElement;
 }
-const landmarkKeys: (keyof LandmarkResults)[] = [
+
+// This list represents the order in which the Landmarks and WorldLandmarks results are returned from each model output.
+// https://ai.google.dev/edge/mediapipe/solutions/vision/pose_landmarker/index#models
+const landmarkKeys: (keyof UnpackedLandmarks)[] = [
   "nose",
   "left_eye_inner",
   "left_eye",
@@ -82,31 +97,36 @@ const landmarkKeys: (keyof LandmarkResults)[] = [
   "right_foot_index",
 ];
 
-function processLandmarkElements(landmarkResults: PoseLandmarkerResult): LandmarkResults {
-  const result: Partial<LandmarkResults> = {};
-  
-  landmarkKeys.forEach((key, index) => {
-    result[key] = {
-      name: landmarkKeys[index],
-      x: landmarkResults.worldLandmarks[0][index].x,
-      y: landmarkResults.worldLandmarks[0][index].y,
-      z: landmarkResults.worldLandmarks[0][index].z,
-      visibility: landmarkResults.worldLandmarks[0][index].visibility,
-    };
-    // LandmarkElement landmarkResults.landmarks[index];
-  });
-  return result as LandmarkResults;
+function processLandmarkElements(landmarkResults: PoseLandmarkerResult): UnpackedLandmarks {
+  const result = {} as UnpackedLandmarks;
+
+  if (Array.isArray(landmarkResults?.worldLandmarks) && landmarkResults.worldLandmarks.length > 0) {
+    // extract all world landmarks from results array in order
+    landmarkKeys.forEach((key, index) => {
+      result[key] = {
+        name: landmarkKeys[index],
+        x: landmarkResults.worldLandmarks[0][index].x,
+        y: landmarkResults.worldLandmarks[0][index].y,
+        z: landmarkResults.worldLandmarks[0][index].z,
+        visibility: landmarkResults.worldLandmarks[0][index].visibility,
+      };
+    });
+  }
+  return result as UnpackedLandmarks;
 }
 
-function calculateKneeAngle(LandmarkResults: LandmarkResults): number {
+function calculateKneeAngle(LandmarkResults: UnpackedLandmarks): number {
   return 0;
 }
 
 export default function PoseDetectionPage() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [parsedLandmarks, setParsedLandmarks] = useState<LandmarkResults | null>(null);
+  const [parsedLandmarks, setParsedLandmarks] = useState<UnpackedLandmarks | null>(null);
   const [postProcessedData, setPostProcessedData] = useState<any>(null); // Replace 'any' with the actual type
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const recordedChunksRef = useRef<Blob[]>([]);
+  const [isRecording, setIsRecording] = useState(false);
 
   let poseLandmarker: PoseLandmarker | null = null;
 
@@ -177,16 +197,40 @@ export default function PoseDetectionPage() {
   useEffect(() => {
     const initializeVideoStream = async () => {
       if (videoRef.current) {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         videoRef.current.srcObject = stream;
+
+        const recorder = new MediaRecorder(stream, { mimeType: "video/mp4" });
+
+        recorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            console.log("Data available:", event.data.size); // Log chunk size
+            recordedChunksRef.current.push(event.data); // Use useRef to accumulate chunks
+          }
+        };
+
+        recorder.onstop = () => {
+          if (recordedChunksRef.current.length > 0) {
+            const blob = new Blob(recordedChunksRef.current, { type: "video/mp4" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "recorded-video.mp4"; // Set the filename
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            recordedChunksRef.current = []; // Clear chunks after saving
+          } else {
+            console.error("No recorded data available");
+          }
+        };
+
+        mediaRecorderRef.current = recorder;
       }
     };
 
     initializeVideoStream();
 
-    // Clean up video stream on component unmount
     return () => {
       if (videoRef.current && videoRef.current.srcObject) {
         const stream = videoRef.current.srcObject as MediaStream;
@@ -198,7 +242,10 @@ export default function PoseDetectionPage() {
 
   // Function to run inference on each video frame
   const detectPose = async () => {
-    if (!poseLandmarker || !videoRef.current || !canvasRef.current) return;
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    if (!poseLandmarker || !video || !canvas) return;
 
     // // Call pose detection
     // const results = poseLandmarker.detectForVideo(
@@ -225,20 +272,22 @@ export default function PoseDetectionPage() {
     //     //     canvasCtx?.fill();
     //     //   });
     //   }
-    canvasRef.current.width = videoRef.current.videoWidth;
-    canvasRef.current.height = videoRef.current.videoHeight;
+    const context = canvas.getContext('2d');
 
-    const canvasCtx = canvasRef.current.getContext("2d");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const canvasCtx = canvas.getContext("2d");
     if (canvasCtx) {
       const drawingUtils = new DrawingUtils(canvasCtx!);
       let lastVideoTime = -1;
 
       let startTimeMs = performance.now();
-      if (lastVideoTime !== videoRef.current.currentTime) {
-        lastVideoTime = videoRef.current.currentTime;
-        poseLandmarker.detectForVideo(videoRef.current, startTimeMs, (result) => {
+      if (lastVideoTime !== video.currentTime) {
+        lastVideoTime = video.currentTime;
+        poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
           canvasCtx.save();
-          canvasCtx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+          canvasCtx.clearRect(0, 0, canvas!.width, canvas!.height);
           for (const landmark of result.landmarks) {
             drawingUtils.drawLandmarks(landmark, {
               radius: (data) => DrawingUtils.lerp(data.from!.z, -0.15, 0.1, 2, 1),
@@ -249,10 +298,12 @@ export default function PoseDetectionPage() {
             });
           }
           setParsedLandmarks(processLandmarkElements(result));
-          console.log(result.worldLandmarks);
           canvasCtx.restore();
         });
       }
+
+      // Draw the video frame onto the canvas so we can save it all in one place
+      canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
       // Process the next frame
       requestAnimationFrame(detectPose);
@@ -292,9 +343,35 @@ export default function PoseDetectionPage() {
     };
   }, []);
 
+  // Start recording
+  const startRecording = () => {
+    if (mediaRecorderRef.current && mediaRecorderRef.current.state === "inactive") {
+      recordedChunksRef.current = []; // Clear previous recordings
+      mediaRecorderRef.current.start(1000); // Start recording with a 1-second timeslice
+      setIsRecording(true);
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current && mediaRecorderRef.current.state === "recording") {
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
+    }
+  };
+
   const LandmarkTable: React.FC<{ landmarks: typeof parsedLandmarks }> = ({ landmarks }) => {
-    console.log("loging landmarks");
-    console.log(landmarks);
+    if (!landmarks) {
+      return (
+        <table>
+          <tbody>
+            <tr>
+              <td>No landmarks detected</td>
+            </tr>
+          </tbody>
+        </table>
+      );
+    }
+  
     return (
       <div>
         <h2>Landmark Positions:</h2>
@@ -308,24 +385,14 @@ export default function PoseDetectionPage() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>{landmarks?.left_ankle.name}</td>
-              <td>{landmarks?.left_ankle.x.toFixed(2)}</td>
-              <td>{landmarks?.left_ankle.y.toFixed(2)}</td>
-              <td>{landmarks?.left_ankle.z.toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>{landmarks?.right_ankle.name}</td>
-              <td>{landmarks?.right_ankle.x.toFixed(2)}</td>
-              <td>{landmarks?.right_ankle.y.toFixed(2)}</td>
-              <td>{landmarks?.right_ankle.z.toFixed(2)}</td>
-            </tr>
-            <tr>
-              <td>{landmarks?.left_knee.name}</td>
-              <td>{landmarks?.left_knee.x.toFixed(2)}</td>
-              <td>{landmarks?.left_knee.y.toFixed(2)}</td>
-              <td>{landmarks?.left_knee.z.toFixed(2)}</td>
-            </tr>
+          {Object.entries(landmarks).map(([name, element]) => (
+          <tr key={name}>
+            <td>{name}</td>
+            <td>{element?.x.toFixed(2)}</td>
+            <td>{element?.y.toFixed(2)}</td>
+            <td>{element?.z.toFixed(2)}</td>
+          </tr>
+        ))}
           </tbody>
         </table>
       </div>
@@ -384,6 +451,14 @@ export default function PoseDetectionPage() {
           marginLeft: "20px", // Adds some space between video and table
         }}
       >
+        <div>
+          <button onClick={startRecording} disabled={isRecording}>
+            Start Recording
+          </button>
+          <button onClick={stopRecording} disabled={!isRecording}>
+            Stop Recording
+          </button>
+        </div>
         {/* Table Component or Div */}
         <LandmarkTable landmarks={parsedLandmarks} />
       </div>
